caeadd794faf543490cdd63d01208efc556b6a3f
proferizzi <47471314+proferizzi@users.noreply.github.com>
2021-06-26 11:47:30 +0200

    Update onda_sinusoidale_conDAC_MCP4725.ino

--- QUARTOanno/onda_sinusoidale_conDAC_MCP4725.ino
+++ QUARTOanno/onda_sinusoidale_conDAC_MCP4725.ino
@@ -362,3 +362,54 @@ void loop(void) {
 */
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+// Esperimento di test per generare manualmente segnali con potenziometro
+
+#include <Wire.h>
+#include <Adafruit_MCP4725.h>
+
+Adafruit_MCP4725 dac;
+
+// Set this value to 9, 8, 7, 6 or 5 to adjust the resolution
+#define DAC_RESOLUTION    (9)
+
+void setup(void) {
+  Serial.begin(9600);  
+  // For Adafruit MCP4725A1 the address is 0x62 (default) or 0x63 (ADDR pin tied to VCC)
+  // For MCP4725A0 the address is 0x60 or 0x61
+  // For MCP4725A2 the address is 0x64 or 0x65
+  // per le schede cinesi è 0x60
+  dac.begin(0x61);   // quella blu 61 le altre 60  
+}
+
+void loop(void) {
+  int val = analogRead(A0);
+  val = map(val, 0, 1023, 0, 4095);
+  // Writes data to the DAC and the EEPROM (persisting the assigned value after reset)
+  dac.setVoltage(val, false);  
+  Serial.println(val);
+  delay(50);
+}
+
+// Fonte Aliverti
+
+*/
+
+
+
+
+
