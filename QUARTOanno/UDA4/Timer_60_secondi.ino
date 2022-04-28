90b9e1b805fe035c251ff9f163e82b78ec52b3d3
proferizzi <47471314+proferizzi@users.noreply.github.com>
2022-04-28 14:59:03 +0200

    Update Timer_60_secondi.ino

--- QUARTOanno/UDA4/Timer_60_secondi.ino
+++ QUARTOanno/UDA4/Timer_60_secondi.ino
@@ -10,10 +10,11 @@ int counter = 0 ;
 void setup() {
   Timer1.initialize();
   MFS.initialize(&Timer1);
-  MFS.write("Hi");delay(2000);MFS.write("123");delay(2000);
-  MFS.write(-203);delay(2000);MFS.write(3.141, 2);delay(2000); 
+  //MFS.write("Hi");delay(2000);MFS.write("123");delay(2000);
+  //MFS.write(-203);delay(2000);MFS.write(3.141, 2);delay(2000); 
 
-  pinMode(3, OUTPUT);
+  pinMode(3, OUTPUT);  
+  counter = 0;
 }
 
 
@@ -22,16 +23,11 @@ void loop() {
     counter++;
     MFS.write(counter);
   }else{
-      tone(3, 220, 100);
-      MFS.write("End");
-      MFS.blinkDisplay(DIGIT_ALL, ON);
-      delay(2000);
-      MFS.blinkDisplay(DIGIT_ALL, OFF);
-      counter = 0;
+    tone(3, 400, 2000);
+    counter = 0;
   }
-  delay(1000);  
+  delay(1000);
 }
 
 // Fonte Aliverti https://www.youtube.com/watch?v=HxjootD0ZOs
-
-
+// https://www.arduino.cc/reference/en/language/functions/advanced-io/tone/
