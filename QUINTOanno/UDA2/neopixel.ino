59c089efe1b089a5c17324715843061bb6f466db
proferizzi <47471314+proferizzi@users.noreply.github.com>
2022-10-21 18:10:00 +0200

    Update neopixel.ino

--- QUINTOanno/UDA2/neopixel.ino
+++ QUINTOanno/UDA2/neopixel.ino
@@ -37,3 +37,64 @@ void loop() {
 }
 
 // Fonte Aliverti https://www.youtube.com/watch?v=V_t4gCYLs9Y&t=49s
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+#include <Adafruit_NeoPixel.h>
+
+#ifdef __AVR__
+  #include <avr/power.h>
+#endif
+
+#define EL 16
+#define PIN 6
+
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(EL, PIN, NEO_GRB + NEO_KHZ800);
+
+void setup() {
+  pixels.begin();
+
+  for (int i = 0; i < EL; i++){
+    pixels.setPixelColor(i, pixels.Color(0,0,0));  
+  }
+  pixels.show();
+  delay(1000);
+}
+
+int c = 0;
+
+void loop() {
+  for (int i = 0; i < EL; i++) {
+    int r = (c % EL);
+    if (i == r) {
+      pixels.setPixelColor(i, pixels.Color(0,120,120));  
+    } else {
+      pixels.setPixelColor(i, pixels.Color(0,0,0));    
+    }    
+  }
+  pixels.show();
+  delay(1000);
+  c++;
+}
+
+
+// Fonte Aliverti https://www.youtube.com/watch?v=txRKpAqDEws
+
+
+*/
