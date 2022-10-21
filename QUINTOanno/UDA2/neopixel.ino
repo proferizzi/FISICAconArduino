d284e820bf3a1e0dbb524406b31a553f67279e68
proferizzi <47471314+proferizzi@users.noreply.github.com>
2022-10-21 18:12:35 +0200

    Update neopixel.ino

--- QUINTOanno/UDA2/neopixel.ino
+++ QUINTOanno/UDA2/neopixel.ino
@@ -1,3 +1,6 @@
+/*
+
+
 // NeoPixel Ring 
 #include <Adafruit_NeoPixel.h>
 
@@ -39,7 +42,7 @@ void loop() {
 // Fonte Aliverti https://www.youtube.com/watch?v=V_t4gCYLs9Y&t=49s
 
 
-
+*/
 
 
 
@@ -55,6 +58,7 @@ void loop() {
 
 
 /*
+// in sequenza
 
 #include <Adafruit_NeoPixel.h>
 
@@ -98,3 +102,78 @@ void loop() {
 
 
 */
+
+
+
+
+
+
+/*
+
+// alba tramonto
+
+// HUE Da 160 (blue) a 6 (giallo) https://github.com/FastLED/FastLED/wiki/FastLED-HSV-Colors
+
+#include <FastLED.h>
+
+#define NUM_LEDS 16
+CRGB leds[NUM_LEDS];
+
+void setup() {
+  Serial.begin(9600);
+  FastLED.addLeds<WS2812, 7, GRB>(leds, NUM_LEDS);
+}
+
+void loop() {
+  
+  int h = 0;
+  int s = 255;
+  //alba
+  for (int i = 0; i < 100; i++) { 
+    //Serial.println(h);
+    h = map(i, 0, 100, 160, 200);   
+    fill_solid(leds, NUM_LEDS, CHSV(h, s, 255));
+    FastLED.show();
+    delay(100);
+  }
+  
+  for (int i = 0; i < 100; i++) { 
+    s = map(i, 0, 100, 255, 0);   
+    fill_solid(leds, NUM_LEDS, CHSV(h, s, 255));
+    FastLED.show();
+    delay(100);
+  }
+  //giorno
+  delay(3000);  
+  h = 64; //giallo
+  s = 0;
+  fill_solid(leds, NUM_LEDS, CHSV(h, s, 255));
+  FastLED.show();
+  //tramonto
+  
+  for (int i = 0; i < 100; i++) { 
+    s = map(i, 0, 100, 0, 255);   
+    fill_solid(leds, NUM_LEDS, CHSV(h, s, 255));
+    FastLED.show();
+    delay(100);
+  }
+  for (int i = 0; i < 100; i++) { 
+    //Serial.println(h);
+    h = map(i, 0, 100, 64, 0);   
+    fill_solid(leds, NUM_LEDS, CHSV(h, s, 255));
+    FastLED.show();
+    delay(50);
+  }
+  for (int i = 0; i < 100; i++) { 
+    //Serial.println(h);
+    h = map(i, 0, 100, 255, 160);   
+    fill_solid(leds, NUM_LEDS, CHSV(h, s, 255));
+    FastLED.show();
+    delay(50);
+  }
+  //notte
+  delay(5000);
+  
+}
+
+// Fonte Aliverti https://www.youtube.com/watch?v=rSlzbtbnyeg
