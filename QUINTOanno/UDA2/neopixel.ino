d3a7dbf7cba0b103ac5ab13d217805d418b7baa7
proferizzi <47471314+proferizzi@users.noreply.github.com>
2022-10-21 18:07:34 +0200

    Create neopixel.ino

---
+++ QUINTOanno/UDA2/neopixel.ino
@@ -0,0 +1,39 @@
+// NeoPixel Ring 
+#include <Adafruit_NeoPixel.h>
+
+#ifdef __AVR__
+  #include <avr/power.h>
+#endif
+
+//pin a cui sono connessi 
+#define PIN 6
+//numero pixel
+#define PIX      16
+
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(PIX, PIN, NEO_GRB + NEO_KHZ800);
+
+void setup() {
+  pixels.begin(); 
+
+  pixels.setPixelColor(0, pixels.Color(0,150,0));
+  pixels.show();
+  delay(1000); 
+
+  //spengo tutto
+}
+
+void loop() {
+  //BLINK
+  for (int i = 0; i < PIX; i++) {
+    pixels.setPixelColor(i, pixels.Color(255,0,0));
+    pixels.show();
+  }
+  delay(1000);
+  for (int i = 0; i < PIX; i++) {
+    pixels.setPixelColor(i, pixels.Color(0,0,0));
+    pixels.show();
+  }
+  delay(1000);
+}
+
+// Fonte Aliverti https://www.youtube.com/watch?v=V_t4gCYLs9Y&t=49s
