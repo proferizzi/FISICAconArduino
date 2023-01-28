6a404c2f975add5c9f81478e9593ab9b50e5ebf2
proferizzi <47471314+proferizzi@users.noreply.github.com>
2023-01-28 14:15:22 +0100

    Update amperometro_conACS712.ino

--- QUINTOanno/UDA1/amperometro_conACS712.ino
+++ QUINTOanno/UDA1/amperometro_conACS712.ino
@@ -1,3 +1,5 @@
+// Sensore ACS712T 05B portata 5 A
+
 void setup() {
   Serial.begin(9600);
 }
@@ -5,21 +7,33 @@ void setup() {
 void loop() {
   //campiono 10 valori e calcolo la media
   int val = 0;
-  for (int i = 0; i < 10; i++) {
+  for (int i = 0; i < 150; i++) {
     val += analogRead(A0);
-    delay(10);
+    delay(3);
   }
-  val = val / 10;
+  val = val / 150;
 
   //ricavo la tensione sul pin A0. 4.88 = 5000 mV / 1024 campioni
-  float tensione = 4.88 * val;
+  //float tensione = 4.88 * val;
 
-  float corrente = (tensione - 2500);
+  float corrente = 1000.0 * (4.88 * val / 1024.0 - 2.5) / 0.185 
+                   + 11581.50 ;  // offset? Vedere caso per caso
+                   // Per uno di essi bisogna togliere anche 25.76
 
-  Serial.print("I = ");
+  //Serial.print("I = ");
   Serial.println(corrente);
   
-  delay(1000);  
+  delay(50);  
 }
 
 // Fonte Aliverti https://www.youtube.com/watch?v=lmfqcHGo85E
+// Fonte https://www.engineersgarage.com/acs712-current-sensor-with-arduino/
+
+// Test 2023 gennaio: 4 resistori in parallelo da 22 Ohm e 250 mW
+//                    con generatore in CC regolabile
+//                    1V esce 103 mA a fronte di 109 mA di amperometro
+//                    2V esce 206 mA a fronte di 212 mA di amperometro
+//                    3V esce 309 mA a fronte di 322 mA di amperometro
+//                    4V esce 412 mA a fronte di 435 mA di amperometro
+//                    (quest'ultimo a 1,74 W escono 435 mW per ogni resistore
+//                    un po' troppo, son belli caldi ma tutto sommato ok)
