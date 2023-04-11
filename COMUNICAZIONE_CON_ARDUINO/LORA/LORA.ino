4fd258fd499ab26d94352c9b66c99677e13233de
proferizzi <47471314+proferizzi@users.noreply.github.com>
2023-04-11 15:01:47 +0200

    Update LORA.ino

--- COMUNICAZIONE_CON_ARDUINO/LORA/LORA.ino
+++ COMUNICAZIONE_CON_ARDUINO/LORA/LORA.ino
@@ -1,4 +1,6 @@
-// Legge la configurazione
+
+
+// Legge la configurazione  N.B. mettere su 3,3 V i due jumper di M0 e M1
 
 #include "Arduino.h"
 #include "LoRa_E32.h"
@@ -79,3 +81,50 @@ void printModuleInformation(struct ModuleInformation moduleInformation) {
 //                https://www.youtube.com/watch?v=_eCTdPFvZsc
 //                https://www.youtube.com/watch?v=6O2C53QfySY
 // Fonte libreria Mischianti  https://www.mischianti.org/2019/10/29/lora-e32-device-for-arduino-esp32-or-esp8266-configuration-part-3
+
+
+
+
+
+
+
+/*
+
+// Ripristina le condizioni di fabbrica  N.B. mettere su 3,3 V i due jumper di M0 e M1
+
+#include "Arduino.h"
+#include "LoRa_E32.h"
+ 
+SoftwareSerial mySerial(3, 2); // e32 TX e32 RX
+ 
+void setup() {
+  Serial.begin(9600);
+  mySerial.begin(9600);
+  
+  delay(500);
+
+  //Factory reset - MO e M1 su H
+  byte buf[] = { 0xC0, 0x00, 0x00, 0x1A, 0x06, 0x44 };
+  mySerial.write((uint8_t *)buf, 6);
+  delay(500);
+    
+  Serial.println("Factory Reset!");  
+}
+ 
+void loop() {
+  if (mySerial.available()) {
+    char c = mySerial.read();
+    Serial.write(c);
+  }
+  
+}
+
+
+
+
+// Fonte Aliverti https://github.com/zmaker/arduino_cookbook/blob/42e6575560c91528fc52b95cf95c65c95019a244/551-Lora_teoria/A551-LORA.pdf
+//                https://www.youtube.com/watch?v=_eCTdPFvZsc
+//                https://www.youtube.com/watch?v=6O2C53QfySY
+// Fonte libreria Mischianti  https://www.mischianti.org/2019/10/29/lora-e32-device-for-arduino-esp32-or-esp8266-configuration-part-3
+
+*/
