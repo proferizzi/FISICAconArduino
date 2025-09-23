8b71d31915700f64631784fe7219ad6061286aa5
proferizzi <47471314+proferizzi@users.noreply.github.com>
2025-09-23 08:25:52 +0200

    Create blink_in_vari_modi.ino

---
+++ SETTIMANE_SCIENZA/2025/blink_in_vari_modi.ino
@@ -0,0 +1,38 @@
+// BLINK IN VARI MODI CON LED PULSANTE POTENZIOMETRO
+
+bool puls = 0;  // Definizione di variabile booleana
+int x = 0;
+unsigned long t0 = 0;
+int stato = 1;  // CAMBIARE VALORE PER CASO ZERO O CASO UNO
+
+
+void setup(){
+  pinMode(7, INPUT_PULLUP);  
+  pinMode(13, OUTPUT);
+  Serial.begin(9600);
+}
+
+
+void loop(){
+  if(!stato){  // CASO 0 comando col pulsante
+    x = 0;
+    puls = digitalRead(7);    
+  }
+  if(stato){  // CASO 1 comando col potenziometro
+    x = analogRead(A0)/50;  
+    Serial.println(x);
+  }
+  
+  digitalWrite(13, !puls); // CASO A bloccante ok sfarfallìo
+  delay(x);                 
+  digitalWrite(13, puls);    
+  delay(x);  
+  
+  //if((millis() - t0) >= x){  // CASO B non bloccante 
+    //t0 = millis();           // non riesco a sfarfallare
+    //if(puls == 0) puls = 1;     
+  //}else{
+    //puls = 0;    
+  //}
+  //digitalWrite(13, puls);
+}
